e74a340ed012664a200101a972add675c2e09a79
Eric Vaish <ericvaish@outlook.com>
2024-09-29 03:56:09 +0530

    Update startup.sh

--- Startup Scripts/startup.sh
+++ Startup Scripts/startup.sh
@@ -1,9 +1,8 @@
-# This is the min requirement. However encountering isue while recording, due to insufficient permission assigned to the External SSD.
 cd /
-sudo chmod u+w /media/ericvaish/Pothole_SSD/Eric_V/Pothole_Recordings
-sudo chmod 777 /dev/sda2
-sudo chmod +x /dev/sda2
+sudo chmod +x /media/ericvaish/Pothole_SSD
+#sudo chmod 777 /dev/sda2
+#sudo chmod +x /dev/sda2
 cd home/ericvaish/Pothole
-sudo python fan.py &
-sudo python record.py &
+python fan.py &
+python record.py &
 cd /
